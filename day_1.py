cb3a653eaf104120167839b42c77be77adb2ef41
Pulatov_Suxrob <pulatovsuhrob007@gmail.com>
2025-12-03 08:14:15 +0500

    1-fayl qo'shildi
    1-dars AI savollariga javoblar

---
+++ day_1.py
@@ -0,0 +1,15 @@
+# s = "12, 5, 7, 3, 20"
+# b = s.split()
+# clean = [int(x.replace(',', '')) for x in b]
+# print(sum(clean))
+
+# s = "adventOfCode!"
+# find_max = max(s)
+# print(find_max)
+
+# s = "hello world"
+# a = s.split()
+# a.reverse()
+# b = " ".join(a)
+# print(b)
+
---
+++ mohirdev_2dars/helloworld#2.py
@@ -0,0 +1,12 @@
+dastur = "Hello World!"
+print(dastur)
+xabar = "Sms keldi"
+print(xabar)
+xabar = "Email keldi"
+xabar = "telegram"
+print(xabar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius ** 2
+print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
